81a6df72433651b7ed296fb7efedf0fe2a368930
Himanshu Balani <85930567+himanshubalani@users.noreply.github.com>
2026-06-30 00:29:48 +0530

    chore: remove tracked trpc dist folder from git history

--- packages/trpc/dist/index.js
+++
@@ -1,3 +0,0 @@
-export { appRouter } from "./router.js";
-export { createContext, router, publicProcedure, authedProcedure, protectedProcedure, workspaceProcedure } from "./trpc.js";
-//# sourceMappingURL=index.js.map
